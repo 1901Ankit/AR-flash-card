f8edc8eb2a92f5fb4ec3d73506de803e59ff7bd9
1901ankit <ank27516@gmail.com>
2026-07-08 19:55:41 +0530

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
 import Home from "./pages/Home";
-import CompileMarker from "./pages/CompileMarker";
 
 function App() {
-  const [showCompiler, setShowCompiler] = useState(false);
-
-  if (showCompiler) {
-    return <CompileMarker />;
-  }
-
-  return (
-    <div className="relative">
-      <Home />
-      <button
-        type="button"
-        onClick={() => setShowCompiler(true)}
-        className="fixed bottom-4 left-4 z-50 px-3 py-1.5 text-xs rounded-full bg-slate-700/80 text-white"
-      >
-        Compile Marker (dev)
-      </button>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
